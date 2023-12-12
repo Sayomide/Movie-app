8a59e0c029fb4ed032acbaa06770dec307aae437
ayomide <mikaheelayomide5@gmail.com>
2023-12-12 12:05:31 +0100

    started work

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html',
+            './dist/movie.js',
+            ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
