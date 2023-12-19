0af4069a35983d12adf2d3c6a96ccbb9ed5d8b6d
ayomide <mikaheelayomide5@gmail.com>
2023-12-19 11:15:09 +0100

    got the api response for use successfully

--- dist/movie.js
+++ dist/movie.js
@@ -1,25 +1,55 @@
-// api response
-api_key = "724bcb77";
+// ACTION FOR WHEN WE SEARCH BUTTON
+ let searchArea = document.querySelector("#search");
+ let theValue;
+ searchArea.addEventListener("blur", () => {
+ theValue = searchArea.value;
+    gettingMovie();
+  let mainBody = document.querySelector("#list");
+  let newchild = document.createElement("div");
+ // newchild.innerHTML = 
+ `
+   <div class="flex flex-row justify-around items-start  w-72 h-10 bg-gray-300 rounded border-b-2 ml-24 mt-2">
+     <img src="noimage.jpg" alt="noimage" class="w-10 h-10 rounded-full p-2">
+      <p class="flex flex-col">Title:<span class="" id="title"></span>
+      </p>
+      <p class="flex flex-col">Year:<span class="" id="year"></span> 
+      </p>
+      <p class="flex flex-col"> Type:<span class="" id="type"></span>
+      </p>
+   </div>
+ `
+ mainBody.appendChild(newchild);
+ });
+  
+ // GETTING THE API RESPONSE
+let api_key = "724bcb77";
 async function gettingMovie() {
-  let response = await fetch(`https://www.omdbapi.com/?s=commando&apikey=${api_key}`);
+  let response = await fetch(`https://www.omdbapi.com/?s=${theValue}&apikey=${api_key}`);
    let data = await response.json();
-  console.log(data.Search);
+//   console.log(data.Search);
+   creatingDiv(data)
   // document.querySelector(".img").src = data.Poster;
 }
-
-// function for search button
-  document.querySelector("#search").addEventListener("keyup", () => {
-    console.log("keybuped")
-    gettingMovie();
-  })
-//  if emterred serch is === poster api results
-
-//function displayall(){}
-// displayall();
-
-
-
-
+  
+ let result;
+// creating the div
+// let a;
+function creatingDiv(data) {
+result = data.Search;
+if(result) {
+// console.log(result)
+result.forEach(eachData => {
+        console.log(eachData.Title);
+   })
+}else {
+  
+}
+  }
+  
+  
+  
+  
+//  if enterred serch is === poster api results
 
 
 
