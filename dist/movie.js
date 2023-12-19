a7cdf62f8a94e6f08b0044c78f6dce5f8415fa0d
ayomide <mikaheelayomide5@gmail.com>
2023-12-19 12:16:45 +0100

    added the gotten api result to search

--- dist/movie.js
+++ dist/movie.js
@@ -1,24 +1,9 @@
 // ACTION FOR WHEN WE SEARCH BUTTON
  let searchArea = document.querySelector("#search");
  let theValue;
- searchArea.addEventListener("blur", () => {
+ searchArea.addEventListener("change", () => {
  theValue = searchArea.value;
     gettingMovie();
-  let mainBody = document.querySelector("#list");
-  let newchild = document.createElement("div");
- // newchild.innerHTML = 
- `
-   <div class="flex flex-row justify-around items-start  w-72 h-10 bg-gray-300 rounded border-b-2 ml-24 mt-2">
-     <img src="noimage.jpg" alt="noimage" class="w-10 h-10 rounded-full p-2">
-      <p class="flex flex-col">Title:<span class="" id="title"></span>
-      </p>
-      <p class="flex flex-col">Year:<span class="" id="year"></span> 
-      </p>
-      <p class="flex flex-col"> Type:<span class="" id="type"></span>
-      </p>
-   </div>
- `
- mainBody.appendChild(newchild);
  });
   
  // GETTING THE API RESPONSE
@@ -26,32 +11,38 @@ let api_key = "724bcb77";
 async function gettingMovie() {
   let response = await fetch(`https://www.omdbapi.com/?s=${theValue}&apikey=${api_key}`);
    let data = await response.json();
-//   console.log(data.Search);
    creatingDiv(data)
   // document.querySelector(".img").src = data.Poster;
 }
   
- let result;
-// creating the div
+let result;
 // let a;
 function creatingDiv(data) {
 result = data.Search;
 if(result) {
 // console.log(result)
 result.forEach(eachData => {
+  let mainBody = document.querySelector("#list");
+  let newchild = document.createElement("div");
+  mainBody.appendChild(newchild);
+  newchild.innerHTML = 
+ `
+   <div class="flex flex-row justify-around items-start  w-96 h-20 bg-gray-300 rounded border-b-2 mt-2">
+     <img src=${eachData.Poster} alt="noimage" class="w-10 h-10 rounded-full p-2">
+      <p class="flex flex-col justify-center items-center m-2"><b>Title:</b><span class="" id="title">${eachData.Title}</span>
+      </p>
+      <p class="flex flex-col justify-center items-center m-2"><b>Year:</b><span class="" id="year">${eachData.Year}</span> 
+      </p>
+      <p class="flex flex-col justify-center items-center m-2"><b>Type:</b><span class="" id="type">${eachData.Type}</span>
+      </p>
+   </div>
+ `
         console.log(eachData.Title);
    })
 }else {
-  
+  // when the result is false
 }
   }
-  
-  
-  
-  
-//  if enterred serch is === poster api results
-
-
 
 
 /*
